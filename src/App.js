ed86558b123578ba631b5da053054d55abdcddae
Adam Yodinsky <adam@zooz.com>
2019-12-27 21:07:37 +0200

    clean up for new ui

--- src/App.js
+++ src/App.js
@@ -1,57 +1,26 @@
-import React, {Component} from 'react';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import MochooButton from './components/MochooButton/MochooBotton';
-import Header from './components/Header/Header';
-import Movie from './components/Movie/Movie';
-import ToolTipFilter from './components/Filters/ToolTipFilter'
-import Filter from './components/Filters/Filter'
-import ToggleFiltersButton from './components/toggleFiltersButton/toggleFiltersButton';
-import {default as configs} from './config/config';
-import axios from 'axios';
-import Marks from './components/Filters/marks';
 
-class App extends Component {
-
-  state = {
-    showMovie: false
-  };
-
-
-  getMovie = async () => {
-
-    const url =  `http://${configs.backEndHost}:${configs.backEndPort}/${configs.backEndApi}/movie`;
-    try {
-      const response = await axios.get(url);
-      // TODO remove this when in prod
-      console.log(response.data);
-      console.log(url);
-      this.setState({
-        movie :response.data,
-        showMovie: true
-      });
-
-    } catch (e) {
-      console.log(e.message);
-    }
-  };
-
-  render() {
-    let movies = null;
-
-    if (this.state.showMovie) {
-      movies = (
-          <div>
-            <Movie  movie={this.state.movie} />
-         </div>
-      )
-    }
-
-    return (
-        <div>
-
-        </div>
-    )
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
