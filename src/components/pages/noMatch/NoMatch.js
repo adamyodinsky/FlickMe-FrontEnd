407b266113c85931bc47091219ef95f393b6571e
Adam Yodinsky <adam@zooz.com>
2020-01-05 14:47:14 +0200

    have a quote api on 404 page.. cooolllll

--- src/components/pages/noMatch/NoMatch.js
+++ src/components/pages/noMatch/NoMatch.js
@@ -4,7 +4,7 @@ import noMatch from './noMatch.module.css'
 const NoMatch = (props) => {
   return <div className={noMatch.noMatch}>
     <h1> 404 PAGE NOT FOUND </h1>
-        <p className={noMatch.text}>"{props.quote.quote}"<br/>{props.quote.author}</p>
+        <p>"{props.quote.quote}"<br/><br/>{props.quote.author}</p>
   </div>
   ;
 };
